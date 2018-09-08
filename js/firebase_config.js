6fbcd0922878ed36ffa08e111e15103d792a6de9
Ramees <salim.ramees@gmail.com>
2018-09-09 02:42:16 +0530

    Login completed

---
+++ js/firebase_config.js
@@ -0,0 +1,10 @@
+
+  var config = {
+    apiKey: "AIzaSyCSFQpl6OzKEU36RpAJ0FQWsVKVGu8x5JI",
+    authDomain: "nss-attendance.firebaseapp.com",
+    databaseURL: "https://nss-attendance.firebaseio.com",
+    projectId: "nss-attendance",
+    storageBucket: "nss-attendance.appspot.com",
+    messagingSenderId: "1089549780858"
+  };
+  firebase.initializeApp(config);
